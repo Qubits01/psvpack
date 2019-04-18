ed18acb47b56bc3ad176e1fa6dcec511e875d0c9
Qubits01 <39989491+Qubits01@users.noreply.github.com>
2019-04-18 23:26:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 
 from setuptools import setup, find_packages
 setup(
-    name = "psvpack",
-    version = "0.1.0",
-    author = "Jacob Hipps",
-    author_email = "jacob@ycnrg.org",
+    name = "psvstore",
+    version = "0.190418",
+    author = "Qubits",
+    author_email = "no@email.com",
     license = "MIT",
     description = "PS Vita pkg fetch tool",
-    keywords = "psvita psv vita pkg2zip",
-    url = "https://git.ycnrg.org/projects/HBREW/psvpack",
+    keywords = "psvita psv vita",
+    url = "https://github.com/Qubits01/psvstore",
 
     packages = find_packages(),
     scripts = [],
@@ -22,8 +22,7 @@ setup(
     },
 
     entry_points = {
-        'console_scripts': [ 'psvpack = psvpack.cli:_main' ],
-        'gui_scripts': [ 'psvpack-qt = psvpack.gui:_gmain' ],
+        'console_scripts': [ 'psvstore = psvstore.cli:_main' ],
     }
 
     # could also include long_description, download_url, classifiers, etc.
